9bae18db58737b81cd66ba702ef688b7816e061c
MrDuckBr <wwalisson2012@gmail.com>
2022-03-12 17:21:27 -0300

    Modificado front e tabela

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-const GERACOES = 5
-const INDIVIDUOS = 4
-const TAXA_CROSSOVER = 0.7
-const TAXA_MUTACAO = 0.01
-const LIMITE_INFERIOR = -10
-const LIMITE_SUPERIOR = 10
-const TAMANHO_BINARIO = 5
+const GERACOES = 5;
+const INDIVIDUOS = 4;
+const TAXA_CROSSOVER = 0.7;
+const TAXA_MUTACAO = 0.01;
+const LIMITE_INFERIOR = -10;
+const LIMITE_SUPERIOR = 10;
+const TAMANHO_BINARIO = 5;
 
 function aptidao(x) {
-  return Math.pow(x, 2) - 3 * x + 4
+  return Math.pow(x, 2) - 3 * x + 4;
 }
 
 class Individuo {
   constructor(x, aptidao, percSelecao) {
-    this.valor = x
-    this.cromossomo = convertToBinary(x)
-    this.aptidao = aptidao
-    this.percSelecao = percSelecao
+    this.valor = x;
+    this.cromossomo = convertToBinary(x);
+    this.aptidao = aptidao;
+    this.percSelecao = percSelecao;
   }
 
   setCromossomo(cromossomo) {
-    this.cromossomo = cromossomo
+    this.cromossomo = cromossomo;
   }
 
   setvalorByBinario(binario) {
-    this.valor = conversorBinarioToDecimal(binario)
+    this.valor = conversorBinarioToDecimal(binario);
   }
 }
 
 function conversorBinarioToDecimal(binario) {
-  let dec = 0
-  let evidencyBit = binario.shift()
+  let dec = 0;
+  let evidencyBit = binario.shift();
   for (let c = 0; c < binario.length; c++) {
-    dec += Math.pow(2, c) * binario[binario.length - c - 1] //calcula para pegar do último ao primeiro
+    dec += Math.pow(2, c) * binario[binario.length - c - 1]; //calcula para pegar do último ao primeiro
   }
 
-  if (evidencyBit == 1) dec *= -1
+  if (evidencyBit == 1) dec *= -1;
 
-  binario.unshift(evidencyBit)
+  binario.unshift(evidencyBit);
 
-  return dec
+  return dec;
 }
 
 function convertToBinary(number) {
-  let num = Math.abs(number)
-  let binary = (num % 2).toString()
+  let num = Math.abs(number);
+  let binary = (num % 2).toString();
   while (num > 1) {
-    num = parseInt(num / 2)
-    binary = (num % 2) + binary
+    num = parseInt(num / 2);
+    binary = (num % 2) + binary;
   }
-  binary = binary.padStart(4, '0')
-  if (number >= 0) binary = `0${binary}`
-  else binary = `1${binary}`
+  binary = binary.padStart(4, "0");
+  if (number >= 0) binary = `0${binary}`;
+  else binary = `1${binary}`;
   //console.log(binary.split(""));
   // console.log(binary);
-  return binary.split('')
+  return binary.split("");
 }
 
 function novaPopulacao(populacao) {
-  populacao = selecao(populacao)
+  populacao = selecao(populacao);
 
   // console.log("seg", populacao);
   //this.numPopulacoes++;
@@ -66,40 +66,40 @@ function novaPopulacao(populacao) {
     (previousValue, currentValue) =>
       previousValue +
       aptidao(conversorBinarioToDecimal(currentValue.cromossomo)),
-    0,
-  )
+    0
+  );
 
   // Calculo da probabilidade
-  let val = 0
-  const novapop = []
+  let val = 0;
+  const novapop = [];
 
   for (const individuo of populacao) {
-    val = conversorBinarioToDecimal(individuo.cromossomo)
-    novapop.push(new Individuo(val, aptidao(val), aptidao(val) / sumFx))
+    val = conversorBinarioToDecimal(individuo.cromossomo);
+    novapop.push(new Individuo(val, aptidao(val), aptidao(val) / sumFx));
   }
 
-  novapop.sort((a, b) => b.percSelecao - a.percSelecao)
+  novapop.sort((a, b) => b.percSelecao - a.percSelecao);
 
-  return novapop
+  return novapop;
 }
 
 function getMelhor(populacao, index1, index2) {
   if (populacao[index1].aptidao >= populacao[index2].aptidao)
-    return populacao[index1]
-  else return populacao[index2]
+    return populacao[index1];
+  else return populacao[index2];
 }
 
 function gerarPopulacao() {
   //numPopulacoes++;
-  let populacao = []
-  const valores = []
-  let totalFn = 0
+  let populacao = [];
+  const valores = [];
+  let totalFn = 0;
   for (let index = 0; index < INDIVIDUOS; index++) {
     const valorAux = Math.floor(
-      Math.random() * (LIMITE_SUPERIOR - LIMITE_INFERIOR) + LIMITE_INFERIOR,
-    )
-    totalFn += aptidao(valorAux)
-    valores.push(valorAux)
+      Math.random() * (LIMITE_SUPERIOR - LIMITE_INFERIOR) + LIMITE_INFERIOR
+    );
+    totalFn += aptidao(valorAux);
+    valores.push(valorAux);
   }
 
   for (let i = 0; i < valores.length; i++) {
@@ -107,76 +107,76 @@ function gerarPopulacao() {
       new Individuo(
         valores[i],
         aptidao(valores[i]),
-        aptidao(valores[i]) / totalFn,
-      ),
-    )
+        aptidao(valores[i]) / totalFn
+      )
+    );
   }
 
-  populacao.sort((a, b) => b.percSelecao - a.percSelecao)
+  populacao.sort((a, b) => b.percSelecao - a.percSelecao);
 
-  return populacao
+  return populacao;
 }
 
 function crossover(pai, mae) {
-  let filho1 = [...pai.cromossomo]
-  let filho2 = [...mae.cromossomo]
+  let filho1 = [...pai.cromossomo];
+  let filho2 = [...mae.cromossomo];
   if (Math.random() <= TAXA_CROSSOVER) {
-    console.log('CROSSOVER')
+    console.log("CROSSOVER");
     // let filho1 = ["1", "8", "8", "8", "8"];
     // let filho2 = ["0", "1", "0", "0", "7"];
     // const index = Math.floor(Math.random() * (TAMANHO_BINARIO - 1) + 1);
 
-    const index = Math.floor(Math.random() * (TAMANHO_BINARIO - 1))
-    const sliceCross1 = filho1.slice(index)
-    const sliceCross2 = [...filho2].slice(index)
+    const index = Math.floor(Math.random() * (TAMANHO_BINARIO - 1));
+    const sliceCross1 = filho1.slice(index);
+    const sliceCross2 = [...filho2].slice(index);
 
-    filho2.splice(index, TAMANHO_BINARIO)
-    filho2 = filho2.concat(sliceCross1)
+    filho2.splice(index, TAMANHO_BINARIO);
+    filho2 = filho2.concat(sliceCross1);
     // filho2;
 
-    filho1.splice(index, TAMANHO_BINARIO)
-    filho1 = filho1.concat(sliceCross2)
+    filho1.splice(index, TAMANHO_BINARIO);
+    filho1 = filho1.concat(sliceCross2);
   }
-  return { filho1, filho2 }
+  return { filho1, filho2 };
 }
 
 function torneio(populacao) {
-  const indexEscolha1 = Math.floor(Math.random() * INDIVIDUOS)
-  let indexEscolha2 = 0
+  const indexEscolha1 = Math.floor(Math.random() * INDIVIDUOS);
+  let indexEscolha2 = 0;
   do {
-    indexEscolha2 = Math.floor(Math.random() * INDIVIDUOS)
-  } while (indexEscolha1 === indexEscolha2)
+    indexEscolha2 = Math.floor(Math.random() * INDIVIDUOS);
+  } while (indexEscolha1 === indexEscolha2);
 
-  const melhor = getMelhor(populacao, indexEscolha1, indexEscolha2)
-  return melhor
+  const melhor = getMelhor(populacao, indexEscolha1, indexEscolha2);
+  return melhor;
 }
 
 function mutacao(param1) {
-  let result = []
+  let result = [];
   //Necessário passar um individuo para o método
 
   for (let i = 0; i < param1.length; i++) {
     if (Math.random() <= TAXA_MUTACAO) {
-      console.log('MUTACAO')
-      if (param1[i] == '1') {
-        result.push('0')
-      } else result.push('1')
-    } else result.push(param1[i])
+      console.log("MUTACAO");
+      if (param1[i] == "1") {
+        result.push("0");
+      } else result.push("1");
+    } else result.push(param1[i]);
   }
 
-  return result
+  return result;
 }
 
 function selecao(populacao) {
-  const pai = torneio(populacao)
-  let mae = torneio(populacao)
-  const filho1 = Object.assign(Object.create(Object.getPrototypeOf(pai)), pai)
-  const filho2 = Object.assign(Object.create(Object.getPrototypeOf(mae)), mae)
-  let cromossomo
+  const pai = torneio(populacao);
+  let mae = torneio(populacao);
+  const filho1 = Object.assign(Object.create(Object.getPrototypeOf(pai)), pai);
+  const filho2 = Object.assign(Object.create(Object.getPrototypeOf(mae)), mae);
+  let cromossomo;
   do {
-    cromossomo = crossover(pai, mae)
-    filho1.setCromossomo(mutacao(cromossomo.filho1))
-    filho2.setCromossomo(mutacao(cromossomo.filho2))
+    cromossomo = crossover(pai, mae);
+    filho1.setCromossomo(mutacao(cromossomo.filho1));
+    filho2.setCromossomo(mutacao(cromossomo.filho2));
   } while (
     !(
       conversorBinarioToDecimal(filho1.cromossomo) >= -10 &&
@@ -186,109 +186,98 @@ function selecao(populacao) {
       conversorBinarioToDecimal(filho2.cromossomo) >= -10 &&
       conversorBinarioToDecimal(filho2.cromossomo) <= 10
     )
-  )
+  );
   // console.log("familia", [pai, mae, filho1, filho2]);
-  return [pai, mae, filho1, filho2]
+  return [pai, mae, filho1, filho2];
 }
 
 function exec() {
-  let n = 1
+  let n = 1;
 
-  let arrayPopulation = []
-  let populacao = gerarPopulacao()
-  arrayPopulation.push(populacao)
+  let arrayPopulation = [];
+  let populacao = gerarPopulacao();
+  arrayPopulation.push(populacao);
   //console.log('1', arrayPopulation[n - 1])
 
   do {
-    populacao = novaPopulacao(arrayPopulation[n - 1])
-    arrayPopulation.push(populacao)
-    renderTable(arrayPopulation[n - 1])
-    n++
+    populacao = novaPopulacao(arrayPopulation[n - 1]);
+    arrayPopulation.push(populacao);
+    renderTable(arrayPopulation[n - 1], n);
+    n++;
     // console.log(n, arrayPopulation[n - 1])
     // console.log(n);
-  } while (n < GERACOES)
-  renderTable(arrayPopulation[n - 1])
-  console.log('fim', arrayPopulation)
+  } while (n < GERACOES);
+  renderTable(arrayPopulation[n - 1], n);
+  console.log("fim", arrayPopulation);
 }
 
-exec()
+exec();
 
-function renderTable(array) {
-  let divMaster = document.querySelector('#myDynamicTable')
-  console.log(divMaster)
+function renderTable(array, n) {
+  let divMaster = document.querySelector("#myDynamicTable");
+  console.log(divMaster);
 
   //retornar uma div
-  divMaster.appendChild(createADiv(array))
+
+  divMaster.appendChild(createTableWithData(array, n));
 }
 
-function createADiv(array) {
-  let div = document.createElement('div')
-  div.appendChild(createTableWithData(array))
-  
-  return div
-}
+function createTableWithData(array, n) {
+  console.log(array);
+  let table = document.createElement("table");
+  let caption = document.createElement("caption");
+  caption.innerHTML = `Geração ${n}`;
+  table.appendChild(caption);
+  table.style.border = "2px solid black";
+  table.appendChild(headTable());
 
-function createTableWithData(array) {
-  console.log(array)
-  let table = document.createElement('table')
-  table.style.border = '2px solid black'
-  table.appendChild(headTable())
+  let tbody = document.createElement("tbody");
+  array.forEach((element, index) => {
+    tbody.appendChild(createTableWithInformation(element, index));
+  });
+  table.appendChild(tbody);
 
-  let tbody = document.createElement('tbody')
-  array.forEach((element,index) => {
-    
-    tbody.appendChild(createTableWithInformation(element,index))
-    
-    
-
-  })
-  table.appendChild(tbody)
-
-  return table
+  return table;
 }
 
 function headTable() {
-  let tr = document.createElement('tr')
+  let tr = document.createElement("tr");
 
-  let th = document.createElement('th')
-  th.innerHTML = 'Individuo'
-  let th2 = document.createElement('th')
-  th2.innerHTML = 'Valor'
-  let th3 = document.createElement('th')
-  th3.innerHTML = 'F(x)'
-  let th4 = document.createElement('th')
-  th4.innerHTML = 'Cromossomo'
+  let th = document.createElement("th");
+  th.innerHTML = "Individuo";
+  let th2 = document.createElement("th");
+  th2.innerHTML = "Valor";
+  let th3 = document.createElement("th");
+  th3.innerHTML = "F(x)";
+  let th4 = document.createElement("th");
+  th4.innerHTML = "Cromossomo";
 
-  tr.appendChild(th)
-  tr.appendChild(th2)
-  tr.appendChild(th3)
-  tr.appendChild(th4)
+  tr.appendChild(th);
+  tr.appendChild(th2);
+  tr.appendChild(th3);
+  tr.appendChild(th4);
 
-  return tr
+  return tr;
 }
 
-function createTableWithInformation(element,index){
-  
-  let tr = document.createElement('tr')
-  let td = document.createElement('td')
-  let td2 = document.createElement('td')
-  let td3 = document.createElement('td')
-  let td4 = document.createElement('td')
+function createTableWithInformation(element, index) {
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  let td2 = document.createElement("td");
+  let td3 = document.createElement("td");
+  let td4 = document.createElement("td");
 
-  
-  td.innerHTML =index
-  td2.innerHTML = element.valor
-  td3.innerHTML = element.aptidao
-  td4.innerHTML = element.cromossomo.join("")
-  
-  
-    tr.appendChild(td)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
-    tr.appendChild(td4)
+  td.innerHTML = index;
+  td2.innerHTML = element.valor;
+  td3.innerHTML = element.aptidao;
+  td4.innerHTML = element.cromossomo.join("");
 
-    return tr
+  tr.appendChild(td);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+
+  return tr;
 }
 
-
-renderTable()
+renderTable();
